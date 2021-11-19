a9a6768f69bf2b92e1225790e768b3e5ecaa9259
Peter Mann <pm@xdd.sk>
2021-11-19 11:28:26 +0100

    experimental user auth

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,3 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Base(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+
+# TODO Example only delete this
+class PersonModel(Base):
+    __tablename__ = "persons_table"
+
+    name = db.Column(db.String(80))
+    age = db.Column(db.Integer)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f"{self.name}:{self.age}"
+
+class User(Base):
+    login = db.Column(db.String(64), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False) # cleartext for now
+
+    # gives admin privileges
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    # gives transport operator privileges
+#    operates = db.Column(db.Integer, db.ForeignKey('transport_company.id'), default=None)
+
+    # gives crew privileges
+#    employed_by = db.Column(db.Integer, db.ForeignKey('transport_company.id'), default=None)
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def auth(self, password):
+        return password == password
--- src/app/views/routes.py
+++ src/app/views/routes.py
@@ -1,7 +1,82 @@
 from app import app
 from app.models import db
-from app.models.person_model import PersonModel
+from app.models import PersonModel
+from app.models import User
+from flask import request, session
+from sqlalchemy.exc import IntegrityError
 
+@app.route('/users')
+def users():
+    users = User.query.all()
+    response = "Login Password<br>"
+    for user in users:
+        response += f"{user.login} {user.password}<br>"
+    return response
+
+@app.route('/user')
+def user():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.get(session['user_id'])
+        return f"Currently logged in as {user.login}"
+    else:
+        return f"Currently logged out"
+
+@app.route('/logout')
+def logout():
+    if session.get('user_id'):
+        del session['user_id']
+        return "Logged out"
+    else:
+        return "Not logged in"
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return """
+            <form action="login" method="post">
+                <input type="text" placeholder="Login" name="login" required>
+                <input type="password" placeholder="Password" name="password" required>
+                <button type="submit">Login</button>
+            </form>
+        """
+    if request.method == 'POST':
+        login = request.form.get("login")
+        password = request.form.get("password")
+        if login is None or password is None:
+            return "Both login and password are required parameters"
+
+        user = User.query.filter_by(login=login).first()
+        if user is None:
+            return "User doesn't exist"
+        if user.auth(password):
+            session['user_id'] = user.id
+            return "Successfully authenticated"
+        else:
+            return "Wrong password"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return """
+            <form action="register" method="post">
+                <input type="text" placeholder="Login" name="login" required>
+                <input type="password" placeholder="Password" name="password" required>
+                <button type="submit">Register</button>
+            </form>
+        """
+    if request.method == 'POST':
+        login = request.form.get("login")
+        password = request.form.get("password")
+        if login is None or password is None:
+            return "Both login and password are required parameters"
+        user = User(login, password)
+        db.session.add(user)
+        try:
+            db.session.commit()
+            return f"Successfully registered {user.login}"
+        except IntegrityError:
+            return f"User '{user}' already exists"
 
 @app.route('/')
 @app.route('/hello')
